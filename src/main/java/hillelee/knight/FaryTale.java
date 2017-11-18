3ba6c9d3ccd1dac4ccceb6116aac7a2cbe2dc0b6
VStankoV <gallardo_666@mail.ru>
2017-11-18 12:27:40 +0200

    L4 context

--- src/main/java/hillelee/knight/FaryTale.java
+++ src/main/java/hillelee/knight/FaryTale.java
@@ -2,29 +2,61 @@ package hillelee.knight;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 public class FaryTale {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext("hillelee");
 //		Knight knight = new Knight(new Quest());
+
+//		System.out.println(context.getBean(Knight.class));
+		System.out.println(context.getBean("knight123"));
 		
-		System.out.println(context.getBean(Knight.class));
+		
+		Knight knight1 = context.getBean(Knight.class);
+		Knight knight2 = context.getBean(Knight.class);
 		
 		
+		System.out.println(knight1 == knight2);
 	}
 }
 
+@Configuration
+class Config {
+	@Bean
+	public Knight knight123(/*@Qualifier("rescueThePrinces")*/ Quest quest) {
+		return new Knight(quest);
+	}
+	
+	@Bean
+	@Primary
+	Quest killTheDragon() {
+		return new Quest("Kill the Dragon");
+	}
+	
+	@Bean
+	Quest rescueThePrinces() {
+		return new Quest("Rescue The Princes");
+	}
+	
+	
+}
+
+
 @Data
-@Component
+//@Component("myKnight")
+//@Scope(SCOPE_PROTOTYPE)
 class Knight {
 	private final Quest quest;
 }
 
 @Data
-@Component
+//@Component
 class Quest {
-	private String task = "Kill the Dragon";
+	private final String task;
 }
